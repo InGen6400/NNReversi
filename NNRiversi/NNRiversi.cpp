9fde7d22dfd7695987628755891ec417265fd714
InGen6400 <38ingen@gmail.com>
2017-01-20 22:49:21 +0900

    入力関係の最適化、入力バグ対策

--- NNRiversi/NNRiversi.cpp
+++ NNRiversi/NNRiversi.cpp
@@ -14,61 +14,100 @@ int main()
 {
 	char tmp[50] = "";
 	Board *mainBoard;
-	char cpuPut = 0, turn = BLACK;
+	char cpuTurn = -2, cpuPut = 0, turn = BLACK, flipCount = 0;
+	char endFlag = FALSE;
 	int x, y;
 
+	printf("設定\n");
+	//CPUの色設定
+	while (cpuTurn == -2)
+	{
+		printf("CPUの色(B:black or W:white or N:NoCpu):");
+		fgets(tmp, sizeof(tmp), stdin);
+		if (tmp[0] == 'B' || tmp[0] == 'b') {
+			cpuTurn = BLACK;
+		}
+		else if (tmp[0] == 'W' || tmp[0] == 'w') {
+			cpuTurn = WHITE;
+		}
+		else if (tmp[0] == 'N' || tmp[0] == 'n') {
+			cpuTurn = -1;
+		}
+		else {
+			printf("おっと、危うくバグるところでした。丁寧に入力していただけるとうれしいです。\n W, B, Nのどれかですよ？\n");
+		}
+	}
+
+	system("cls");
 	mainBoard = Board_New();
 	Board_Draw(mainBoard);
-	while (1) {
-		fgets(tmp, sizeof(tmp), stdin);
-		if (tmp[0] == 'q')break;
-		if (tmp[0] == 'b') {
-			if (*(mainBoard->Sp-1) != -2) {
-				Board_Undo(mainBoard);
-				Board_Undo(mainBoard);
-				system("cls");
-				printf("戻しました\n");
-				Board_Draw(mainBoard);
-			}
-			else {
-				printf("これ以上戻せません\n");
+	while (!endFlag) {
+		if (turn == cpuTurn) {
+			//CPUのターン
+			printf("CPU Thinking...");
+			NegaMaxSearch(mainBoard, FALSE, turn, 0, &cpuPut);
+			x = getX(cpuPut);
+			y = getY(cpuPut);
+		}
+		else {
+			//プレイヤーのターン
+			while (1) {
+				fgets(tmp, sizeof(tmp), stdin);
+				if (tmp[0] == 'q') {
+					endFlag = TRUE;
+					break;
+				}
+				else if (tmp[0] == '.' && tmp[1] == '.') {
+					if (*(mainBoard->Sp - 1) != -2) {
+						Board_Undo(mainBoard);
+						Board_Undo(mainBoard);
+						system("cls");
+						printf("戻しました\n");
+						Board_Draw(mainBoard);
+					}
+					else {
+						printf("これ以上戻せません\n");
+					}
+					continue;
+				}
+				else if (Player_Input(tmp, &x, &y) == FALSE) {
+					printf("おっと、危うく停止するところでした。\n変な文字は入れないでくださいね\n");
+					continue;
+				}
+				break;
 			}
 		}
+
+		if (x >= 1 && y >= 1 && x <= BOARD_SIZE && y <= BOARD_SIZE) {
+			system("cls");
+			if (flipCount = Board_Flip(mainBoard, turn, x, y) >= 1) {
+				if (turn == cpuTurn) {
+					printf("CPU Put (%c, %c)\n", "ABCDEFGH"[x - 1], "12345678"[y - 1]);
+				}
+				else {
+					printf("You Put (%c, %c)\n", "ABCDEFGH"[x - 1], "12345678"[y - 1]);
+				}
+				printf("裏返した石数:%d\n", flipCount);
+				turn = getOppStone(turn);
+			}
+			else {
+				printf("You Can't Put (%c, %c)\n", "ABCDEFGH"[x - 1], "12345678"[y - 1]);
+			}
+			Board_Draw(mainBoard);
+
+		}
+		/*
 		else 
 		{
 			if (Player_Input(tmp, &x, &y)) {
-				if (x >= 1 && y >= 1 && x <= BOARD_SIZE && y <= BOARD_SIZE) {
-					system("cls");
-					if (Board_Flip(mainBoard, turn, x, y) >= 1) {
-						if (turn == WHITE) {
-							printf("白 Put (%d, %d)\n", x, y);
-						}
-						else {
-							printf("黒 Put (%d, %d)\n", x, y);
-						}
-						turn = getOppStone(turn);
-						NegaMaxSearch(mainBoard, FALSE, turn, 0, &cpuPut);
-						if (turn == WHITE) {
-							x = getX(cpuPut);
-							y = getY(cpuPut);
-							printf("CPU : %d,%d(%d)\n", x, y, cpuPut);
-							printf("flip:%d\n", Board_Flip(mainBoard, turn, x, y));
-							turn = getOppStone(turn);
-						}
-					}
-					else {
-						printf("You Can't Put (%d, %d)\n", x, y);
-					}
-					Board_Draw(mainBoard);
+				
 				}
 				else {
 					printf("You Can't Place that position\n");
 				}
 			}
-			else {
-				printf("おっと、危うく停止するところでした。\n変な文字は入れないでくださいね\n");
-			}
-		}
+			
+		}*/
 	}
 	Board_Delete(mainBoard);
     return 0;
--- NNRiversi/Player.cpp
+++ NNRiversi/Player.cpp
@@ -11,23 +11,25 @@ int Player_Input(char *Input, int *x, int *y) {
 	strcpy(cpy, Input);
 	int ret = 0;
 
-	if (strlen(cpy) >= 3) {
+	if (strlen(cpy) == 4) {
 		tmp = strtok(cpy, " ,.-\t/:;");
 		if ((tmp[0] >= 'a' && tmp[0] <= 'h') || (tmp[0] >= 'A' && tmp[0] <= 'H')) {
 			*x = CharToPos(tmp[0]);
 		}
-		else if (isalnum(tmp[0])) {
+		else if (tmp[0] >= '1' && tmp[0] <= '8') {
 			*y = tmp[0] - '0';
 		}
 		else {
 			return FALSE;
 		}
 
-		tmp = strtok(NULL, " ,.-\t/:;");
+		if ((tmp = strtok(NULL, " ,.-\t/:;")) == '\0') {
+			return FALSE;
+		}
 		if ((tmp[0] >= 'a' && tmp[0] <= 'h') || (tmp[0] >= 'A' && tmp[0] <= 'H')) {
 			*x = CharToPos(tmp[0]);
 		}
-		else if (isalnum(tmp[0])) {
+		else if (tmp[0] >= '1' && tmp[0] <= '8') {
 			*y = tmp[0] - '0';
 		}
 		else {
@@ -37,6 +39,7 @@ int Player_Input(char *Input, int *x, int *y) {
 	else {
 		return FALSE;
 	}
+	return TRUE;
 }
 
 //A~H ‚ð 1~8‚É‚·‚é

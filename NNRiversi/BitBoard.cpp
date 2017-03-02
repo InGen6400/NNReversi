d9c764a3b20128d8e88a0159d301e86df14eda14
InGen6400 <38ingen@gmail.com>
2017-03-02 16:06:41 +0900

    テスト成功

--- NNRiversi/BitBoard.cpp
+++ NNRiversi/BitBoard.cpp
@@ -28,11 +28,11 @@ void BitBoard_Delete(BitBoard *bitboard) {
 
 //盤面の初期化
 void BitBoard_Reset(BitBoard *bitboard) {
-	//bitboard->stone[WHITE] = 0x0000001008000000;//真ん中二つ以外0
-	//bitboard->stone[BLACK] = 0x0000000810000000;//真ん中二つ以外0
-	bitboard->stone[WHITE] = 0xc000100000000000;//
-	bitboard->stone[BLACK] = 0x00c0200000000000;//
-	//角のインデックスが11022012になるはず
+	bitboard->stone[WHITE] = 0x0000001008000000;//真ん中二つ以外0
+	bitboard->stone[BLACK] = 0x0000000810000000;//真ん中二つ以外0
+	//bitboard->stone[WHITE] = 0xc000800000000000;//
+	//bitboard->stone[BLACK] = 0x00c0400000000000;//
+	//角のインデックスが22011021になるはず
 	bitboard->Sp = bitboard->Stack;
 }
 

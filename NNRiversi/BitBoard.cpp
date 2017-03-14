05867e0ba17b8aee0593b37415de632e54f2df2e
InGen6400 <38ingen@gmail.com>
2017-03-14 11:10:48 +0900

    なぜか_lzcnt_u64がntzをカウントしてくるので63-lzcntにしたら完成

--- NNRiversi/BitBoard.cpp
+++ NNRiversi/BitBoard.cpp
@@ -146,18 +146,27 @@ inline int ntz(uint64 in) {
 	return __popcnt64((in&(-in)) - 1);
 }
 
-inline __m256i nonzero(__m256i in) {
+inline __m256i nonzero256(__m256i in) {
 	return _mm256_add_epi64(_mm256_cmpeq_epi64(in, _mm256_setzero_si256()), _mm256_set1_epi64x(1));
 }
 
-inline uint64 h_or(__m256i in) {
+inline __m128i nonzero128(__m128i in) {
+	return _mm_add_epi64(_mm_cmpeq_epi64(in, _mm_setzero_si128()), _mm_set1_epi64x(1));
+}
+
+inline uint64 h_or256(__m256i in) {
 	__m128i tmp = _mm_or_si128(_mm256_extractf128_si256(in, 0), _mm256_extractf128_si256(in, 1));
 	return _mm_extract_epi64(tmp, 0) | _mm_extract_epi64(tmp, 1);
 }
 
+inline uint64 h_or128(__m128i in) {
+	return _mm_extract_epi64(in, 0) | _mm_extract_epi64(in, 1);
+}
+
 //”½“]‚·‚éƒrƒbƒg‚ğ•Ô‚·(—v‚‘¬‰»)
 inline uint64 getReverseBits(const uint64 *me, const uint64 *opp, const uint64 pos) {
-
+	//and
+	//
 #ifdef __AVX2__	
 	//player's stones
 	__m256i mes = _mm256_set1_epi64x(*me);
@@ -174,7 +183,7 @@ inline uint64 getReverseBits(const uint64 *me, const uint64 *opp, const uint64 p
 	//outflank
 	__m256i outf = _mm256_and_si256(_mm256_and_si256(mask, _mm256_add_epi64(_mm256_or_si256(oppM, _mm256_andnot_si256(mask, _mm256_set1_epi16(0xFFFF))), _mm256_set1_epi64x(1))), mes);
 	//will flip
-	__m256i flip = _mm256_and_si256(mask, _mm256_sub_epi64(outf, nonzero(outf)));
+	__m256i flip = _mm256_and_si256(mask, _mm256_sub_epi64(outf, nonzero256(outf)));
 
 
 	//DOWN RIGHT D_LEFT D_RIGHT
@@ -193,14 +202,62 @@ inline uint64 getReverseBits(const uint64 *me, const uint64 *opp, const uint64 p
 
 	//flip = flip | ((-outf << 1) & mask)
 	flip = _mm256_or_si256(flip, _mm256_and_si256(_mm256_slli_epi64(_mm256_sub_epi64(_mm256_setzero_si256(), outf), 1), mask));
-
 	//horizontal or 64x4
-	return h_or(flip);
+	return h_or256(flip);
 #elif __AVX__
 
-#endif
-	
 
+	//player's stones
+	__m128i mes = _mm_set1_epi64x(*me);
+	//Masked Opp
+	__m128i oppM0 = _mm_and_si128(_mm_set1_epi64x(*opp), _mm_set_epi64x(0xFFFFFFFFFFFFFFFF, 0x7E7E7E7E7E7E7E7E));
+	__m128i oppM1 = _mm_and_si128(_mm_set1_epi64x(*opp), _mm_set_epi64x(0x7E7E7E7E7E7E7E7E, 0x7E7E7E7E7E7E7E7E));
+
+	int posCnt = ntz(pos);
+	//mask for UP LEFT
+	__m128i mask0 = _mm_slli_epi64(_mm_set_epi64x(
+		0x0101010101010100ULL,
+		0x00000000000000FEULL), posCnt);
+	//mask for U_RIGHT U_LEFT
+	__m128i mask1 = _mm_slli_epi64(_mm_set_epi64x(
+		0x0002040810204080ULL,
+		0x8040201008040200ULL), posCnt);
+
+	//outflank
+	__m128i outf0 = _mm_and_si128(_mm_and_si128(mask0, _mm_add_epi64(_mm_or_si128(oppM0, _mm_andnot_si128(mask0, _mm_set1_epi16(0xFFFF))), _mm_set1_epi64x(1))), mes);
+	__m128i outf1 = _mm_and_si128(_mm_and_si128(mask1, _mm_add_epi64(_mm_or_si128(oppM1, _mm_andnot_si128(mask1, _mm_set1_epi16(0xFFFF))), _mm_set1_epi64x(1))), mes);
+	//will flip
+	__m128i flip0 = _mm_and_si128(mask0, _mm_sub_epi64(outf0, nonzero128(outf0)));
+	__m128i flip1 = _mm_and_si128(mask1, _mm_sub_epi64(outf1, nonzero128(outf1)));
+
+
+	//DOWN RIGHT
+	mask0 = _mm_srli_epi64(_mm_set_epi64x(
+		0x0080808080808080ULL,
+		0x7F00000000000000ULL), (63 - posCnt));
+	//D_LEFT D_RIGHT
+	mask1 = _mm_srli_epi64(_mm_set_epi64x(
+		0x0102040810204000ULL,
+		0x0040201008040201ULL), (63 - posCnt));
+	//outf = (MSB1 >> lzcnt(~oppM & mask)) & me
+	alignas(64) uint64 AN0[2];
+	alignas(64) uint64 AN1[2];
+	__m128i *andnot0 = (__m128i*)AN0;
+	__m128i *andnot1 = (__m128i*)AN1;
+	*andnot0 = _mm_andnot_si128(oppM0, mask0);
+	*andnot1 = _mm_andnot_si128(oppM1, mask1);
+
+	//AVX‚É‚Í•À—ñ‚ÅLZCNT‚·‚éˆ—‚ª‚È‚¢‚Ì‚Å”z—ñ‚É“WŠJ‚µ‚Älacnt()
+	outf0 = _mm_and_si128(_mm_set_epi64x(0x8000000000000000 >> (63 - _lzcnt_u64(AN0[1])), 0x8000000000000000 >> (63 - _lzcnt_u64(AN0[0]))), mes);
+	outf1 = _mm_and_si128(_mm_set_epi64x(0x8000000000000000 >> (63 - _lzcnt_u64(AN1[1])), 0x8000000000000000 >> (63 - _lzcnt_u64(AN1[0]))), mes);
+
+	//flip = flip | ((-outf << 1) & mask)
+	flip0 = _mm_or_si128(flip0, _mm_and_si128(_mm_slli_epi64(_mm_sub_epi64(_mm_setzero_si128(), outf0), 1), mask0));
+	flip1 = _mm_or_si128(flip1, _mm_and_si128(_mm_slli_epi64(_mm_sub_epi64(_mm_setzero_si128(), outf1), 1), mask1));
+
+	//horizontal or 64x4
+	return (h_or128(flip0) | h_or128(flip1));
+#endif
 	
 	/*
 	uint64 revBits = 0;
@@ -408,7 +465,7 @@ char BitBoard_CountFlips(const uint64 me, const uint64 ene, char pos) {
 	return BitBoard_CountStone(reverse);
 }
 
-void drawBits(uint64 bits) {
+void drawBits(const uint64 bits) {
 	int x;	
 	printf("\n\n");
 	printf(" @@@@‚`  ‚a  ‚b  ‚c  ‚d  ‚e  ‚f  ‚g          \n");
